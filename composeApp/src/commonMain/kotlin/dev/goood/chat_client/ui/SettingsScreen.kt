9e69e4f9b7279b61d770e6f2d39d4b8b24c2c3cc
sdimka <s_d_a@mail.ru>
2025-04-24 19:18:38 +0300

    lil imp;

--- composeApp/src/commonMain/kotlin/dev/goood/chat_client/ui/SettingsScreen.kt
+++ composeApp/src/commonMain/kotlin/dev/goood/chat_client/ui/SettingsScreen.kt
@@ -2,8 +2,11 @@ package dev.goood.chat_client.ui
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -13,17 +16,39 @@ import dev.goood.chat_client.ui.composable.CButton
 
 
 @Composable
-fun SettingsScreen() {
+fun SettingsScreen(
+    modifier: Modifier = Modifier
+) {
     Column(
-        verticalArrangement = Arrangement.Center,
+        verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.fillMaxSize().background(Color(0xFFEFEFEF))
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color.Blue)
     ) {
-        Text("Settings Screen")
 
-        CButton(
-            text = "Logout",
-            onClick = { /* Handle logout logic */ }
+        // Box 1: 20% height
+        Box(
+            modifier = Modifier // Start a new modifier chain for this specific Box
+                .fillMaxWidth()
+                .weight(0.20f) // 20% of parent height
+                .background(Color.Yellow)
+        )
+
+        // Box 2: 35% height
+        Box(
+            modifier = Modifier // New chain
+                .fillMaxWidth()
+                .weight(0.35f) // 35% of parent height
+                .background(Color.Green)
+        )
+
+        // Box 3: 45% height
+        Box(
+            modifier = Modifier // New chain
+                .fillMaxWidth()
+                .weight(0.45f) // 45% of parent height
+                .background(Color.LightGray)
         )
     }
 }
