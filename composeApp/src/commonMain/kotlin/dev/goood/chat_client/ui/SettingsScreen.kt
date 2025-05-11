58420c417329cbfe99a0711b5ff5d8e9945e8939
SDimka <s_d_a@mail.ru>
2025-05-11 12:55:27 +0300

    translate screen

--- composeApp/src/commonMain/kotlin/dev/goood/chat_client/ui/SettingsScreen.kt
+++ composeApp/src/commonMain/kotlin/dev/goood/chat_client/ui/SettingsScreen.kt
@@ -4,10 +4,17 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.selection.SelectionContainer
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
@@ -19,7 +26,16 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalClipboardManager
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import compose.icons.LineAwesomeIcons
+import compose.icons.lineawesomeicons.ArrowRightSolid
+import compose.icons.lineawesomeicons.Copy
+import compose.icons.lineawesomeicons.CopySolid
+import compose.icons.lineawesomeicons.PlaySolid
+import dev.goood.chat_client.ui.composable.BallProgerssIndicator
 import dev.goood.chat_client.ui.composable.CButton
 import dev.goood.chat_client.ui.theme.buttonBackground
 import dev.goood.chat_client.viewModels.TranslateViewModel
@@ -32,8 +48,6 @@ fun SettingsScreen(
 ) {
 
     val viewModel: TranslateViewModel = koinViewModel()
-
-    var inputValue by remember { mutableStateOf("") }
 
     Column(
         verticalArrangement = Arrangement.Top,
@@ -58,33 +72,10 @@ fun SettingsScreen(
                 .weight(0.35f) // 35% of parent height
                 .background(Color.Green)
         ) {
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = modifier.fillMaxWidth()
-            ) {
-                TextField(
-                    value = inputValue,
-                    onValueChange = { inputValue = it },
-                    label = { Text("") },
-                    colors = TextFieldDefaults.colors(
-                        focusedIndicatorColor = buttonBackground,
-                        unfocusedIndicatorColor = buttonBackground,
-                        focusedLabelColor = buttonBackground
-                    ),
-                    trailingIcon = {
-                        CButton(
-                            text = "Send",
-                            onClick = { viewModel.translate() }
-                        )
-                    },
-                    modifier = modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 5.dp)
-                        .padding(vertical = 10.dp)
-                )
-
-
-            }
+            TranslateInput(
+                viewModel = viewModel,
+                modifier = modifier
+            )
         }
 
         // Box 3: 45% height
@@ -93,6 +84,128 @@ fun SettingsScreen(
                 .fillMaxWidth()
                 .weight(0.45f) // 45% of parent height
                 .background(Color.LightGray)
+        ){
+            TranslateOutput(
+                viewModel = viewModel,
+                modifier = modifier
+            )
+        }
+    }
+}
+
+@Composable
+fun TranslateInput(
+    viewModel: TranslateViewModel,
+    modifier: Modifier = Modifier
+) {
+
+    val inputValue by viewModel.fromString.collectAsStateWithLifecycle()
+    val source by viewModel.sourceLanguage.collectAsStateWithLifecycle()
+    val target by viewModel.targetLanguage.collectAsStateWithLifecycle()
+    val state by viewModel.state.collectAsStateWithLifecycle()
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.fillMaxWidth()
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = modifier
+                .padding(top = 5.dp)
+                .fillMaxWidth()
+        ) {
+            Text(source.name)
+            Button(
+                onClick = { viewModel.changeDirection() },
+                colors = ButtonDefaults.buttonColors(Color.Transparent),
+                modifier = modifier.padding(horizontal = 15.dp)
+            ){
+                Icon(
+                    imageVector = LineAwesomeIcons.ArrowRightSolid,
+                    contentDescription = null,
+                    tint = Color.LightGray
+                )
+            }
+            Text(target.name)
+        }
+        TextField(
+            value = inputValue,
+            onValueChange = { viewModel.setInput(it) },
+            label = { Text("") },
+            colors = TextFieldDefaults.colors(
+                focusedIndicatorColor = buttonBackground,
+                unfocusedIndicatorColor = buttonBackground,
+                focusedLabelColor = buttonBackground
+            ),
+            trailingIcon = {
+                Box(
+                    modifier = modifier.padding(end = 5.dp)
+                ) {
+                    when (state) {
+                        TranslateViewModel.TranslateState.Loading -> {
+                            BallProgerssIndicator()
+                        }
+
+                        else -> {
+                            CButton(
+                                icon = LineAwesomeIcons.PlaySolid,
+                                onClick = { viewModel.translate() }
+                            )
+                        }
+                    }
+                }
+
+            },
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(horizontal = 5.dp)
+                .padding(vertical = 10.dp)
         )
+
+
+    }
+}
+
+@Composable
+fun TranslateOutput(
+    viewModel: TranslateViewModel,
+    modifier: Modifier = Modifier
+) {
+
+    val result by viewModel.resultString.collectAsStateWithLifecycle()
+    val clipboardManager = LocalClipboardManager.current
+
+    Column(
+        modifier = modifier.fillMaxWidth()
+    ){
+        Row(
+            horizontalArrangement = Arrangement.End,
+            modifier = modifier
+                .padding(all = 5.dp)
+                .fillMaxWidth()
+        ) {
+            Button(
+                onClick = {
+                    clipboardManager.setText(AnnotatedString(result))
+                },
+                modifier = modifier
+                    .height(40.dp),
+                colors = ButtonDefaults.buttonColors(Color(0xFF274C77)),
+                shape = RoundedCornerShape(8.dp)
+            ){
+                Icon(
+                    imageVector = LineAwesomeIcons.CopySolid,
+                    contentDescription = null,
+                    tint = Color.LightGray
+                )
+            }
+        }
+        SelectionContainer {
+            Text(
+                result,
+                modifier = modifier.padding(horizontal = 5.dp)
+            )
+        }
     }
 }
--- composeApp/src/commonMain/kotlin/dev/goood/chat_client/viewModels/TranslateViewModel.kt
+++ composeApp/src/commonMain/kotlin/dev/goood/chat_client/viewModels/TranslateViewModel.kt
@@ -6,6 +6,8 @@ import dev.goood.chat_client.core.network.Api
 import dev.goood.chat_client.model.Lang
 import dev.goood.chat_client.model.TranslateData
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.launch
 import kotlinx.serialization.json.Json
 import org.koin.core.component.KoinComponent
@@ -15,28 +17,51 @@ class TranslateViewModel: ViewModel(), KoinComponent {
 
     private val api: Api by inject()
 
-    val direction = MutableStateFlow(0)
+    private val _state = MutableStateFlow<TranslateState>(TranslateState.Success)
+    val state = _state.asStateFlow()
+    val sourceLanguage = MutableStateFlow(Lang.EN)
+    val targetLanguage = MutableStateFlow(Lang.RU)
     val fromString = MutableStateFlow("")
-    val toString = MutableStateFlow("")
+    val resultString = MutableStateFlow("Result will be here")
 
-    val state = MutableStateFlow<TranslateState?>(null)
+    fun setInput(resStr: String) {
+        fromString.value = resStr
+    }
 
     fun translate() {
-        val data = TranslateData(
+        val message = TranslateData(
             text = fromString.value,
-            sourceLanguageCode = Lang.EN,
-            targetLanguageCode = Lang.RU
+            sourceLanguageCode = sourceLanguage.value,
+            targetLanguageCode = targetLanguage.value
         )
-        val js = Json.encodeToString(data)
+        val js = Json.encodeToString(message)
         print(js)
-//        viewModelScope.launch {
-//
-//        }
+        viewModelScope.launch {
+            _state.value = TranslateState.Loading
+            api.translateApi.translate(message)
+                .catch {
+                    _state.value = TranslateState.Error(it.message?: "Unknown error")
+                }
+                .collect { res ->
+                    resultString.value = res.translatedText[0]
+                    _state.value = TranslateState.Success
+                }
+        }
+    }
+
+    fun changeDirection() {
+        if (sourceLanguage.value == Lang.EN) {
+            sourceLanguage.value = Lang.RU
+            targetLanguage.value = Lang.EN
+        } else {
+            sourceLanguage.value = Lang.EN
+            targetLanguage.value = Lang.RU
+        }
     }
 
     sealed interface TranslateState {
-        data class Success(val token: String): TranslateState
-        data class Error(val message: String): TranslateState
+        data object Success: TranslateState
+        data class Error(val mess: String): TranslateState
         data object Loading: TranslateState
     }
 }
